641b27b6d65f56bc9de069eb2ce56e2774d4af9f
fabioh2m <fabioh2m@hotmail.com>
2025-10-30 18:45:31 -0300

    Curso de Python
    primeiros programas em .py

---
+++ ex002/index.py
@@ -0,0 +1,8 @@
+msg = 'Olá, Mundo!'
+print(msg)
+nome = input ('Qual é o seu nome?')
+print ('Olá', nome, 'é um prazer te conhecer!')
+dia = input ('Em que dia você nasceu?')
+mes = input ('Em que mês mocê nasceu?')
+ano = input ('Eem que ano você nesceu?')
+print ('Você é:',nome, 'nasceu no dia', dia, 'de', mes, 'de', ano, '. Correto?')
