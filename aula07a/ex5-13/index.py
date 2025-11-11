0b5f6203e418f5c065cbed41d77ef5479c6a0ab7
fabioh2m <fabioh2m@hotmail.com>
2025-11-11 06:41:55 -0300

    python curso em vídeo
    ex05

---
+++ aula07a/ex5-13/index.py
@@ -0,0 +1,7 @@
+#Exercício 1) faça um program que leia um número inteiro e mostre na tela o seu sucessor e antecessor.
+
+# n1= int(input('digite um número:'))
+# print ('O valor digitado foi: {} \n seu sucessor é: {} \n seu antecessor é: {}'. format(n1, n1+1, n1-1))
+
+n1 = int(input('digit um número:'))
+print (f'o número digitado foi: {n1}\n o sucessor é: {n1+1} \n oantecessor é: {n1-1}')
